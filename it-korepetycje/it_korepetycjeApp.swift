6e34a29c306139bc6e3ddfbd233a4beb3d1c4770
Jakub Chudzik <jakubchudzik02@gmail.com>
2021-12-31 09:21:50 +0100

    Initial Commit

---
+++ it-korepetycje/it_korepetycjeApp.swift
@@ -0,0 +1,17 @@
+//
+//  it_korepetycjeApp.swift
+//  it-korepetycje
+//
+//  Created by Jakub Chudzik on 31/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct it_korepetycjeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
